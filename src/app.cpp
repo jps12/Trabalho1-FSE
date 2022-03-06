c06afb55e22cbf746787bcebee719977b340f101
jps12 <jps12carvalho@gmailcom>
2022-03-06 14:59:05 -0300

    chore: confiuguring to get temperature

--- src/app.cpp
+++ src/app.cpp
@@ -7,11 +7,14 @@
 #include <signal.h>
 #include <algorithm>
 
+#define TEMP_INTERNA 0xC1
+#define TEMP_POTENCIOMETRO 0xC2
+
 
 void encerra_execucao(int exit_code){
     fecha_UART();
     printf("Encerrando a execução do programa...\n");
-    exit_code = std::min(exit_code, 2);
+    exit_code = std::min(exit_code, 1);
     exit(exit_code);
 }
 
@@ -22,28 +25,34 @@ int conta_bytes_lidos(unsigned char *buffer){
     return i < 256? i : -1;
 }
 
-void solicita_int(){
-    unsigned char buffer[5];
+void solicita_temperatura(int sub_codigo){
+    unsigned char buffer[9];
     unsigned char *p_buffer;
     p_buffer = &buffer[0];
     *p_buffer++ = (char) 0x01;
     *p_buffer++ = (char) 0x23;
-    *p_buffer++ = (char) 0xA1;
+    *p_buffer++ = (char) sub_codigo;
+    *p_buffer++ = (char) 3;
+    *p_buffer++ = (char) 7;
+    *p_buffer++ = (char) 4;
+    *p_buffer++ = (char) 3;
 
-    configura_CRC(buffer, 5);
+    configura_CRC(buffer, 9);
 
     printf("Buffers de memória criados!\n");
     
-    escreve_mensagem(&buffer[0], 5);
+    escreve_mensagem(&buffer[0], 9);
 
     sleep(1);
 
     unsigned char *resposta_buffer = recebe_resposta();
     // int resposta_tam = conta_bytes_lidos(resposta_buffer);
-    int resposta ;
+    float resposta;
     memcpy(&resposta, resposta_buffer+3, 4);
 
-    printf("O inteiro recebido foi: %d\n", resposta);
+    printf("A temperatura recebida foi: %f\n", resposta);
+
+    free(resposta_buffer);
 }
 
 void menu(){
@@ -51,8 +60,8 @@ void menu(){
     {
         int opcao = -1;
         printf("Escolha o numero da opção que deseja:\n");
-        printf("[1] Receber um int:\n");
-        printf("[2] Receber um float:\n");
+        printf("[1] Solicita temperatura interna:\n");
+        printf("[2] Solicita temperatura do potenciometro:\n");
         printf("[3] Receber uma string:\n");
         printf("[4] Enviar um int:\n");
         printf("[5] Enviar um float:\n");
@@ -64,7 +73,10 @@ void menu(){
         switch (opcao)
         {
         case 1:
-            solicita_int();
+            solicita_temperatura(TEMP_INTERNA);
+            break;
+        case 2:
+            solicita_temperatura(TEMP_POTENCIOMETRO);
             break;
         case 7:
             puts("Finalizando o programa\n");
